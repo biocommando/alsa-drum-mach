bcd3dd28bb9760e920588c6c4b003f554763446e
biocommando <joonas1000@gmail.com>
2022-02-20 22:31:19 +0200

    improvements to kit config generation

--- create_drum_mach_conf.py
+++ create_drum_mach_conf.py
@@ -5,10 +5,13 @@ import sys
 
 dirname = './'
 if len(sys.argv) > 1:
-    dirname = sys.argv[-1] + '/'
+    dirname = sys.argv[-1]
+if not dirname.endswith('/'):
+    dirname = dirname + '/'
 
 files = os.listdir(dirname)
-files = list(filter(lambda x: x.endswith('.wav'), files))
+files = list(filter(lambda x: x.endswith('.wav') or x.endswith('.WAV'), files))
+files = sorted(files)
 
 conf = '''. Automatically generated drum machine config file
 I common_conf.vars 0
@@ -27,15 +30,24 @@ conf = conf + f'{{slots}}  {len(files)}\n'
 
 slot_id = 0
 cc_num = 0
+slot_names = []
+
 for file in files:
     conf = conf + '\n'
     slot_name = re.sub(' ', '_', file)
     if slot_name != file:
-        os.rename(file, slot_name)
+        os.rename(dirname + file, dirname + slot_name)
         file = slot_name
     slot_name = re.sub('.wav', '', slot_name)
+    slot_name = re.sub('.WAV', '', slot_name)
     if len(slot_name) > 15:
         slot_name = slot_name[:15]
+    i = 0
+    orig_slot_name = slot_name
+    while slot_name in slot_names:
+    	slot_name = (str(i) + orig_slot_name)[:15]
+    	i = i + 1
+    slot_names.append(slot_name)
     conf = conf + f'{{set_var}}   {slot_name}       {slot_id}\n'
     conf = conf + f'{{load}}      {{{slot_name}}}     {file}\n'
     conf = conf + f'{{cc}}        {{{slot_name}}}     {cc_num}                 {{speed_min}} {{speed_max}}\n'
